57b223f13222f7ba23f3dfe14005385a9256a86c
OpaSliviSozreli <segol144@gmail.com>
2024-10-11 15:08:46 +0300

    +reading from file

--- processor.cpp
+++ processor.cpp
@@ -15,93 +15,109 @@ Errors run()
 
     size_t file_size = count_file_size( fp );
 
-    char* buff = ( char * ) calloc( file_size, sizeof( char ) );
-    fread( buff, sizeof( char ), file_size, fp );
+    int* code = ( int* ) calloc( file_size, sizeof( int ) );
+    fread( code, sizeof( char ), file_size, fp );
 
     fclose( fp );
 
     while( 1 )
     {
-        int cmd = 0;
-        scanf( "%d", cmd );
+        int ip = 0;
 
-        if ( cmd == PUSH )
+        if ( code[ip] == PUSH )
         {
             stack_element_t arg = 0;
             scanf( "%d", &arg );
             stack_push( &stk, arg );
+            ip+=2;
         }
 
-        else if ( cmd == SUB )
+        else if ( code[ip] == SUB )
         {
             stack_element_t stack_value_1 = stack_pop( &stk );
             stack_element_t stack_value_2 = stack_pop( &stk );
 
             stack_push( &stk, stack_value_2 - stack_value_1 );
+            ip++;
         }
 
-        else if ( cmd == ADD )
+        else if ( code[ip] == ADD )
         {
             stack_element_t stack_value_1 = stack_pop( &stk );
             stack_element_t stack_value_2 = stack_pop( &stk );
 
             stack_push( &stk, stack_value_2 + stack_value_1 );
+            ip++;
         }
 
-        else if ( cmd == MUL )
+        else if ( code[ip] == MUL )
         {
             stack_element_t stack_value_1 = stack_pop( &stk );
             stack_element_t stack_value_2 = stack_pop( &stk );
 
             stack_push( &stk, stack_value_2 * stack_value_1 );
+            ip++;
         }
 
-        else if ( cmd == DIV )
+        else if ( code[ip] == DIV )
         {
             stack_element_t stack_value_1 = stack_pop( &stk );
             stack_element_t stack_value_2 = stack_pop( &stk );
 
             stack_push( &stk, stack_value_2 / stack_value_1 );
+            ip++;
         }
 
-        else if ( cmd == SQRT )
+        else if ( code[ip] == SQRT )
         {
             stack_element_t stack_value = stack_pop( &stk );
 
             stack_push( &stk, sqrt( stack_value ) );
+            ip++;
         }
 
-        else if ( cmd == SIN )
+        else if ( code[ip] == SIN )
         {
             stack_element_t stack_value = stack_pop( &stk );
 
             stack_push( &stk, sin( stack_value ) );
+            ip++;
         }
 
-        else if ( cmd == COS )
+        else if ( code[ip] == COS )
         {
             stack_element_t stack_value = stack_pop( &stk );
 
             stack_push( &stk, cos( stack_value ) );
+            ip++;
         }
 
-        else if ( cmd == OUT )
+        else if ( code[ip] == OUT )
+        {
             fprintf( stderr, "%d", stack_pop( &stk ) );
+            ip++;
+        }
 
-        else if ( cmd == IN )
+        else if ( code[ip] == IN )
         {
             stack_element_t arg = 0;
             scanf( "%d", &arg );
             stack_push( &stk, arg );
+            ip += 2;
         }
 
-        else if ( cmd == DUMP )
+        else if ( code[ip] == DUMP )
+        {    
             STACK_DUMP( &stk );
+            ip++;
+        }
 
-        else if ( cmd == HLT )
-            break;
+        else if ( code[ip] == HLT )
+        {    break;
+            ip++;
+        }
         else
-            fprintf( stderr, "STNTERR: '%s'", cmd );
+            fprintf( stderr, "STNTERR: '%s'", code[ip] );
     }
 }
 
